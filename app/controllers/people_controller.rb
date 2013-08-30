fc35ef89580f9c063c4e26107b13270e80d8b526
Stacey Vetzal <stacey@vetzal.com>
2013-08-30 10:25:13 -0400

    initial commit

---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,31 @@
+class PeopleController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  respond_to 'json'
+
+  def create
+    person = Person.create(person_params)
+    respond_with person
+  end
+
+  def update
+    person = Person.find(params[:id])
+    person.update_attributes!(person_params)
+    respond_with person
+  end
+
+  def destroy
+    begin
+      person = Person.find(params[:id])
+      person.destroy!
+      respond_with nil
+    rescue
+      render :text => :nil, :status => :not_found
+    end
+  end
+
+  private
+
+  def person_params
+    params.require(:person).permit(:first_name, :last_name, :phone, :email)
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ResTest::Application.config.secret_key_base = '6faf56a49049cc4fbf998e29836768a5f7354e855f1c91f2b67a7bfa96db4c137ca6a252e1c9a3d2eab1fea985460ce667419f9bcb4df5bad0e08b8d13be2b16'
