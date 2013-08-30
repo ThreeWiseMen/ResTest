114fa2fe1c1ef66b489828a0c7d9fa7c1e915a75
Stacey Vetzal <stacey@vetzal.com>
2013-08-30 17:09:46 -0400

    adding capistrano, unicorn, setting up for initial deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,43 +20,21 @@ set :use_sudo, false
 
 set :branch, 'master'
 
-task :external do
-  set :gateway, "office.threewisemen.ca"
-end
-
-task :production do
-  puts "\n\nProduction not supported. Use Engine Yard."
-  exit 0
-end
-
-task :staging do
-  role :app, "10.1.2.XXX"
-  role :web, "10.1.2.XXX"
-  role :db,  "10.1.2.XXX", :primary => true
-  set :rails_env, 'staging'
-end
+role :app, "restest.threewisemen.ca"
+role :web, "restest.threewisemen.ca"
+role :db,  "restest.threewisemen.ca", :primary => true
+set :rails_env, 'production'
 
 namespace :deploy do
   task :start do
-    run "$HOME/start.sh"
+    run "cd #{current_path} && bundle exec unicorn --config-file #{current_path}/config/unicorn.#{rails_env}.conf.rb --daemonize --env #{rails_env}"
   end
   task :stop do
-    run "$HOME/stop.sh"
-    sleep 8
+    run "if [ -e #{deploy_to}/shared/pids/unicorn.pid ]; then cat #{deploy_to}/shared/pids/unicorn.pid* | xargs kill; fi"
   end
   task :restart do
-    stop
-    start
+    run "if [ -e #{deploy_to}/shared/pids/unicorn.pid ]; then cat #{deploy_to}/shared/pids/unicorn.pid* | xargs kill -s USR2; fi"
   end
-#  task :start do
-#    run "cd #{current_path} && bundle exec unicorn --config-file #{current_path}/config/unicorn.#{rails_env}.conf.rb --daemonize --env #{rails_env}"
-#  end
-#  task :stop do
-#    run "if [ -e #{deploy_to}/shared/pids/unicorn.pid ]; then cat #{deploy_to}/shared/pids/unicorn.pid* | xargs kill; fi"
-#  end
-#  task :restart do
-#    run "if [ -e #{deploy_to}/shared/pids/unicorn.pid ]; then cat #{deploy_to}/shared/pids/unicorn.pid* | xargs kill -s USR2; fi"
-#  end
 
   task :full do
     update
---
+++ config/unicorn.production.conf.rb
@@ -0,0 +1,39 @@
+APP_HOME = '/home/reaction/Reaction'
+worker_processes 2
+
+working_directory "#{APP_HOME}/current" # available in 0.94.0+
+
+listen '127.0.0.1:3003', :tcp_nopush => true
+timeout 60
+
+pid "#{APP_HOME}/shared/pids/unicorn.pid"
+
+stderr_path "#{APP_HOME}/shared/log/unicorn.stderr.log"
+stdout_path "#{APP_HOME}/shared/log/unicorn.stdout.log"
+
+preload_app true
+
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect!
+
+  # # This allows a new master process to incrementally
+  # phase out the old master process with SIGTTOU to avoid a
+  # thundering herd (especially in the "preload_app false" case)
+  # when doing a transparent upgrade.  The last worker spawned
+  # will then kill off the old master process with a SIGQUIT.
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+
+  # *optionally* throttle the master from forking too quickly by sleeping
+  sleep 1
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+end
