ca73a94a76e3a392d8642aa9222fe8392b2580be
Stacey Vetzal <stacey@vetzal.com>
2013-09-03 13:06:19 -0400

    synthesizing person template for id 1

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,11 +2,11 @@ class PeopleController < ApplicationController
   respond_to 'json'
 
   def show
-    begin
-      person = Person.find(params[:id])
-      respond_with person
-    rescue
+    person = find_person
+    if person.nil?
       render :text => :nil, :status => :not_found
+    else
+      respond_with person
     end
   end
 
@@ -16,14 +16,14 @@ class PeopleController < ApplicationController
   end
 
   def update
-    person = Person.find(params[:id])
+    person = Person.find(person_id)
     person.update_attributes!(person_params)
     respond_with person
   end
 
   def destroy
     begin
-      person = Person.find(params[:id])
+      person = Person.find(person_id)
       person.destroy!
       respond_with nil
     rescue
@@ -35,5 +35,31 @@ class PeopleController < ApplicationController
 
   def person_params
     params.require(:person).permit(:first_name, :last_name, :phone, :email)
+  end
+
+  def person_id
+    params[:id].to_i
+  end
+
+  def person_sample
+    Person.new(
+      :id => 1,
+      :first_name => "Stacey",
+      :last_name => "Vetzal",
+      :phone => "905-555-1212",
+      :email => "stacey@threewisemen.ca"
+    )
+  end
+
+  def find_person
+    person = nil
+    begin
+      person = Person.find(person_id)
+    rescue
+      # We want to fake out a person for ID 1 so there's
+      # always one in the db to reference for tests.
+      person = person_sample if person_id == 1
+    end
+    return person
   end
 end
--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PeopleControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Retrieve template person" do
+    get :show, { :id => 1, :format => 'json'}
+    assert_response :success
+
+    person = JSON.parse(response.body)
+    assert_equal("Stacey", person['first_name'])
+    assert_equal("Vetzal", person['last_name'])
+    assert_equal("905-555-1212", person['phone'])
+    assert_equal("stacey@threewisemen.ca", person['email'])
+    assert_equal(1, person['id'])
+  end
 end
